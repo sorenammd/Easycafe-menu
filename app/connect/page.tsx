ff52e01cfa346a86cdde957a93bcfb62cf1cc315
sorenammd <dev.mohammadii@gmail.com>
2026-06-04 17:57:31 +0330

    Enhance button styling and add SVG icon for improved visual appeal in ConnectPage

--- app/connect/page.tsx
+++ app/connect/page.tsx
@@ -90,8 +90,20 @@ export default function ConnectPage() {
                                     </p>
                                     <p className="mt-1 text-sm leading-6 text-white/54">{item.note}</p>
                                 </div>
-                                <span className="rounded-full border border-orange-500/18 bg-orange-500/10 px-3 py-1 text-sm font-semibold text-orange-100/88">
-                                    باز کن
+                                <span style={{ opacity: '0.4' }} className="grid place-content-center rounded-xl  min-h-10
+                                px-3 py-1 text-sm font-semibold
+                                 text-orange-100/88">
+                                    <svg className="h-6 w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+                                        stroke="#f5f4f4">
+                                        <g id="SVGRepo_bgCarrier" strokeWidth="0" />
+                                        <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" />
+                                        <g id="SVGRepo_iconCarrier">
+                                            <path d="M21 9.00001L21 3.00001M21 3.00001H15M21 3.00001L12 12M10 
+                                        3H7.8C6.11984 3 5.27976 3 4.63803 3.32698C4.07354 3.6146 3.6146 4.07354 3.32698 4.63803C3 5.27976 3 
+                                        6.11984 3 7.8V16.2C3 17.8802 3 18.7202 3.32698 19.362C3.6146 19.9265 4.07354 20.3854 4.63803 20.673C5.27976
+                                         21 6.11984 21 7.8 21H16.2C17.8802 21 18.7202 21 19.362 20.673C19.9265 20.3854 20.3854 19.9265 20.673 19.362C21 18.7202 21 17.8802 21 16.2V14" stroke="#fffafa" stroke-width="2" strokeLinecap="round" strokeLinejoin="round"></path>
+                                        </g>
+                                    </svg>
                                 </span>
                             </a>
                         ))}
